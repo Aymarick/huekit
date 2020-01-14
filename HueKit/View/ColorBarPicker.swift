c981c97c7ed880406ece62763eba47e88baedcf0
Aymeric De Abreu <ada@appouest.com>
2020-01-14 16:22:44 +0100

    ⚙️Fix Swift 5 automatic adjustment.

--- HueKit/View/ColorBarPicker.swift
+++ HueKit/View/ColorBarPicker.swift
@@ -254,7 +254,7 @@ open class ColorBarPicker: UIControl {
 		get {
 			var t = super.accessibilityTraits
 			
-			t |= UIAccessibilityTraits.adjustable
+			t.insert(UIAccessibilityTraits.adjustable)
 			
 			return t
 		}
